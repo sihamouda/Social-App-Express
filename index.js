5f8c69692577c63eb840fabeb8dfe9d6136a76ff
sihamouda <anis.c.hamouda@gmail.com>
2022-11-12 01:38:01 +0100

    Pug

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ var express = require("express");
 //create application
 var app = express();
 
+//import Pug
+app.set("view engine", "pug");
+app.set("views", "./views");
+
 //import routes
 var things = require("./things.js");
 
 //both index.js and things.js should be in same directory
 app.use("/things", things);
 
-app.get("/:id", function (req, res) {
-  res.send("id: " + req.params.id);
-});
+//dynamic routes
+// app.get("/:id", function (req, res) {
+//   res.send("id: " + req.params.id);
+// });
 
+//middleware
 app.use("/", function (req, res, next) {
   console.log("A request for things received at " + Date.now());
   next();
@@ -21,6 +27,13 @@ app.use("/", function (req, res, next) {
 
 app.get("/", function (req, res) {
   res.send("hello");
+});
+
+//Pug template for specific route
+app.get("/first_template", function (req, res) {
+  res.render("first_view", {
+    name: "Anis",
+  });
 });
 
 app.listen(3000);
